34dffa141c74cccab07a74dcf6cc428b64f05b18
mkx <han109k@gmail.com>
2021-04-12 10:17:42 +0300

    js file added

---
+++ CandyMuseum/script.js
@@ -0,0 +1,8 @@
+// scroll funtion for navbar
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
